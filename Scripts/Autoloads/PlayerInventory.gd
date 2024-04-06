d7de92cd65da46609699e3d8f92d621f6253627b
EveningComet <moist@debian>
2024-04-06 11:37:35 -0400

    I decided to make an autoload for the player's inventory. This is a single player game, so there's no problem with that.

---
+++ Scripts/Autoloads/PlayerInventory.gd
@@ -0,0 +1,4 @@
+## The player's inventory.
+extends Inventory
+
+var starting_money_on_new_game: int = 100
--- Scripts/Inventory/PlayerInventory.gd
+++
@@ -1,5 +0,0 @@
-## Houses items.
-# TODO: Make this an autoload?
-class_name PlayerInventory extends Node
-
-var stored_items: Array[ItemSlotData] = []
