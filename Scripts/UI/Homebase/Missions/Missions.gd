66ad13a3e8cc822eb39e2f5d75b238af27e2a3d4
thanh <33893929+yenthanhmm5@users.noreply.github.com>
2024-04-09 09:35:53 +0200

    Connect missions scene from home base

--- Scripts/UI/Homebase/Missions/Missions.gd
+++ Scripts/UI/Homebase/Missions/Missions.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export_file("*.tscn") var battle_scene: String
+
 @export var level_label: Label
 @export var container: VBoxContainer
 @export var prev_level_button: BaseButton
@@ -38,7 +40,11 @@ func make_button(text: String) -> BaseButton:
 	var button = Button.new()
 	button.text = text
 	button.custom_minimum_size = minimum_button_size
+	button.button_down.connect( on_mission_button_pressed )
 	return button
+	
+func on_mission_button_pressed() -> void:
+	SceneController.switch_to_scene( battle_scene )
 
 func remove_all_mission_buttons() -> void:
 	for button in get_tree().get_nodes_in_group(mission_button_group_name):
