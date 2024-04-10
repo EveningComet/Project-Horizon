f02b85cbf31e677c7b91a8e0b67b8b88a59d24f7
thanh <33893929+yenthanhmm5@users.noreply.github.com>
2024-04-10 20:38:28 +0200

    Add enemy, mission, and show description

--- Scripts/UI/Homebase/Missions/Missions.gd
+++ Scripts/UI/Homebase/Missions/Missions.gd
@@ -2,32 +2,30 @@ extends Control
 
 @export_file("*.tscn") var battle_scene: String
 
-@export var unlocked_label: Label
-@export var container: VBoxContainer
+@export var buttons_container: VBoxContainer
+@export var mission_description: Label
+@export var description_panel: Panel
 @export var missions: Array[MissionData]
 
 const mission_button_group_name: String = "missions"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_unlocked_label()
 	add_mision_buttons()
-
-func set_unlocked_label() -> void:
-	unlocked_label.text = "Unlocked: "
-	unlocked_label.text += str( MissionController.number_of_missions_completed() )
-	unlocked_label.text += "/" + str( len(missions) )
+	description_panel.hide()
 
 func add_mision_buttons() -> void:
 	for mission in missions:
 		var is_unlocked = MissionController.is_mission_unlocked( mission )
 		var button = make_button( mission, is_unlocked )
 		button.add_to_group( mission_button_group_name )
-		container.add_child( button )
+		buttons_container.add_child( button )
 
 func make_button(mission: MissionData, is_unlocked: bool) -> MissionSelectButton:
 	var button = MissionSelectButton.new( mission, is_unlocked )
 	button.button_down.connect( on_mission_button_pressed.bind( button.mission ) )
+	button.mouse_entered.connect( on_mouse_entered_mission_button.bind( button.mission ) )
+	button.mouse_exited.connect( on_mouse_exited_mission_button.bind( button.mission ) )
 	return button
 	
 func on_mission_button_pressed(mission: MissionData) -> void:
@@ -36,3 +34,11 @@ func on_mission_button_pressed(mission: MissionData) -> void:
 	if OS.is_debug_build() == true:
 		print("Missions :: Selected mission ", MissionController.current_mission.mission_name,
 			" with nr of enemies=", len( MissionController.current_mission.enemies ))
+	
+func on_mouse_entered_mission_button(mission: MissionData) -> void:
+	mission_description.text = mission.mission_description
+	description_panel.show()
+
+func on_mouse_exited_mission_button(mission: MissionData) -> void:
+	description_panel.hide()
+
