93eafe83891b8b1b2085551e3b056fbb5052d3cc
thanh <33893929+yenthanhmm5@users.noreply.github.com>
2024-04-12 15:55:56 +0200

    Add confirm/undo logic to points

--- Scripts/UI/Skill/SkillMenu.gd
+++ Scripts/UI/Skill/SkillMenu.gd
@@ -2,6 +2,7 @@ class_name SkillMenu extends Control
 
 @export var tab_bar: TabBar
 @export var exit_button: Button
+@export var confirm_button: Button
 @export var undo_skill_points_button: Button
 @export var skill_container: HBoxContainer
 @export var skill_points_label: Label
@@ -11,12 +12,15 @@ const skills_group_name := "skills"
 
 var characters:= PlayerPartyController.party_members
 var current_character: PlayerCombatant
+var draft_available_skill_points
 
 func _ready():
 	add_tabs_per_character()
 	visibility_changed.connect( on_visibility_changed )
 	tab_bar.tab_changed.connect( render_tab )
 	exit_button.button_down.connect( hide )
+	confirm_button.button_down.connect( confirm_points )
+	undo_skill_points_button.button_down.connect( undo_points )
 
 func add_tabs_per_character():
 	tab_bar.clear_tabs()
@@ -31,8 +35,16 @@ func on_visibility_changed():
 func render_tab(index: int):
 	current_character = characters[index]
 	show_skills()
-	check_available_skill_points()
-	set_available_skill_points_label()
+	set_available_skill_points( current_character.available_skill_points )
+
+func confirm_points():
+	current_character.available_skill_points = draft_available_skill_points
+	set_available_skill_points( current_character.available_skill_points )
+	get_tree().call_group( skills_group_name, "confirm" )
+
+func undo_points():
+	set_available_skill_points( current_character.available_skill_points )
+	get_tree().call_group( skills_group_name, "undo" )
 
 func show_skills():
 	clear_skills()
@@ -48,17 +60,29 @@ func make_skill_button(skill: SkillInstance) -> SkillMenuButton:
 	return button
 
 func on_skill_upgraded():
-	current_character.available_skill_points -= 1
+	undo_skill_points_button.disabled = false
+	confirm_button.disabled = false
+	set_available_skill_points( draft_available_skill_points - 1 )
 	set_available_skill_points_label()
-	check_available_skill_points()
 
-func check_available_skill_points():
-	if (current_character.available_skill_points == 0):
-		get_tree().call_group( skills_group_name, "disable" )
+func set_available_skill_points(new_value: int):
+	draft_available_skill_points = new_value
+	set_available_skill_points_label()
+	disable_skills_if_no_points_left()
+	disable_confirm_and_undo_if_no_action_taken()
 
 func set_available_skill_points_label():
 	skill_points_label.text = "Available skill points: "
-	skill_points_label.text += str( current_character.available_skill_points )
+	skill_points_label.text += str( draft_available_skill_points )
+
+func disable_skills_if_no_points_left():
+	var function_name = "disable" if draft_available_skill_points == 0 else "enable"
+	get_tree().call_group( skills_group_name, function_name )
+
+func disable_confirm_and_undo_if_no_action_taken():
+	var no_action_taken: bool = draft_available_skill_points == current_character.available_skill_points
+	confirm_button.disabled = no_action_taken
+	undo_skill_points_button.disabled = no_action_taken
 
 func clear_skills():
 	for child in skill_container.get_children():
--- Scripts/UI/Skill/SkillMenuButton.gd
+++ Scripts/UI/Skill/SkillMenuButton.gd
@@ -1,6 +1,8 @@
 class_name SkillMenuButton extends TextureButton
 
 var skill: SkillInstance
+var draft_level
+
 @export var level_label: Label
 
 signal skill_upgraded()
@@ -19,17 +21,27 @@ func enable():
 	unapply_grayscale()
 
 func upgrade_skill():
-	skill.current_upgrade_level += 1
+	set_upgrade_level( draft_level + 1 )
 	set_level_text()
-	emit_signal("skill_upgraded")
+	emit_signal( "skill_upgraded" )
+
+func confirm():
+	skill.current_upgrade_level = draft_level
+
+func undo():
+	set_upgrade_level( skill.current_upgrade_level )
 
 func set_correct_texture_and_text():
 	texture_normal = skill.monitored_skill.display_texture
 	tooltip_text = get_formatted_skill_text()
+	set_upgrade_level( skill.current_upgrade_level )
+
+func set_upgrade_level(new_value: int):
+	draft_level = new_value
 	set_level_text()
 
 func set_level_text():
-	level_label.text = str(skill.current_upgrade_level) + "/" + str(skill.monitored_skill.max_rank)
+	level_label.text = str( draft_level ) + "/" + str( skill.monitored_skill.max_rank )
 
 func get_formatted_skill_text() -> String:
 	var text = skill.monitored_skill.localization_name + ": "
