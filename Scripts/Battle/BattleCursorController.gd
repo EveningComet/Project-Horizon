b2d0a380ff90d6de7c2d1e8063701072e770c6b9
EveningComet <moist@debian>
2024-04-12 13:06:10 -0400

    I fixed single target selection in the battle scene.

--- Scripts/Battle/BattleCursorController.gd
+++ Scripts/Battle/BattleCursorController.gd
@@ -18,6 +18,9 @@ var spawned_cursors: Array = []
 ## The characters that are the current targets.
 var targets: Array[Combatant] = []
 
+## Used for single target actions.
+var curr_index: int = 0
+
 ## Spawns the needed cursors based on the passed action.
 func spawn_needed_cursors(new_action: StoredAction) -> void:
 	match new_action.action_type:
@@ -27,7 +30,7 @@ func spawn_needed_cursors(new_action: StoredAction) -> void:
 			canvas_layer.add_child( cursor )
 			
 			# Set the target to the first enemy
-			# TODO: More robust system for remembering the previously selected target?
+			# TODO: More robust system for remembering the previously selected target.
 			set_target_for_cursor(cursor, enemy_battle_ui.enemy_party_container.get_child(0), true )
 			target_scan_node = enemy_battle_ui.enemy_party_container
 		
@@ -45,6 +48,7 @@ func spawn_needed_cursors(new_action: StoredAction) -> void:
 			canvas_layer.add_child( cursor )
 			
 			# Set the target to be the first ally
+			# TODO: Caching the previous target for convenience.
 			set_target_for_cursor(cursor, player_battle_ui.player_party_container.get_child(0), true )
 			target_scan_node = player_battle_ui.player_party_container
 		
@@ -61,6 +65,7 @@ func clear_cursors() -> void:
 		cursor.queue_free()
 	spawned_cursors.clear()
 	targets.clear()
+	curr_index = 0
 
 func set_target_for_cursor(cursor, battler_ui: CombatantBattleUI, overrides: bool) -> void:
 	if overrides == true:
@@ -70,6 +75,18 @@ func set_target_for_cursor(cursor, battler_ui: CombatantBattleUI, overrides: boo
 	targets.append( new_target )
 	cursor.global_position = battler_ui.global_position + Vector2(5, 5)
 
+func find_closest_target_to_mouse(mouse_pos: Vector2) -> void:
+	var potential_target: CombatantBattleUI = null
+	var target_dist: float = INF
+	for battler: CombatantBattleUI in target_scan_node.get_children():
+		var dist = mouse_pos.distance_to(battler.global_position)
+		if dist < target_dist:
+			potential_target = battler
+			target_dist      = dist
+	if targets.has(potential_target.get_combatant()) == true:
+		return
+	set_target_for_cursor( spawned_cursors[0], potential_target, true )
+
 ## Find the closest target for a cursor. This method should only do stuff when
 ## there is one cursor.
 func find_closest_target(direction: Vector2) -> void:
@@ -77,19 +94,21 @@ func find_closest_target(direction: Vector2) -> void:
 	if spawned_cursors.size() > 1:
 		return
 	
-	# TODO: Upgrade this to allow selecting a target with the gamepad.
-	var selected_target: CombatantBattleUI = null
-	var target_distance: float             = INF
-	var curr_pos: Vector2 = spawned_cursors[0].global_position
-	for battler in target_scan_node.get_children():
-		var dist = curr_pos.distance_to(battler.global_position)
-		if dist < target_distance:
-			selected_target = battler
-			target_distance = dist
+	if direction.x > 0 or direction.y > 0:
+		curr_index += 1
+	elif direction.x < 0 or direction.y < 0:
+		curr_index -= 1
 	
-	if selected_target != null:
-		# Set the cursor's position
-		set_target_for_cursor( spawned_cursors[0], selected_target, true )
+	if curr_index > target_scan_node.get_child_count() - 1:
+		curr_index = 0
+	elif curr_index < 0:
+		curr_index = target_scan_node.get_child_count() - 1
+	
+	var selected_target: CombatantBattleUI = target_scan_node.get_child( curr_index )
+	if targets.has(selected_target.get_combatant()) == true:
+		return
+	
+	set_target_for_cursor( spawned_cursors[0], selected_target, true)
 
 ## Accessor for getting the targets.
 func get_targets() -> Array[Combatant]:
