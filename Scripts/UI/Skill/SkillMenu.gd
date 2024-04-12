1c5c3d8ac2acde26fc2ba5f5a5fdfb95741f7b4a
thanh <33893929+yenthanhmm5@users.noreply.github.com>
2024-04-12 14:27:58 +0200

    Add skill current level, available points, and undo button

--- Scripts/UI/Skill/SkillMenu.gd
+++ Scripts/UI/Skill/SkillMenu.gd
@@ -2,9 +2,9 @@ class_name SkillMenu extends Control
 
 @export var tab_bar: TabBar
 @export var exit_button: Button
+@export var undo_skill_points_button: Button
 @export var skill_container: HBoxContainer
-@export var skill_description_label: RichTextLabel
-
+@export var skill_points_label: Label
 @export var skill_menu_button_template: PackedScene
 
 var characters:= PlayerPartyController.party_members
@@ -13,27 +13,36 @@ var characters:= PlayerPartyController.party_members
 func _ready():
 	add_tabs_per_character()
 	visibility_changed.connect( on_visibility_changed )
-	tab_bar.tab_changed.connect( show_skills )
+	tab_bar.tab_changed.connect( render_tab )
 	exit_button.button_down.connect( hide )
-
-func on_visibility_changed():
-	if (visible):
-		show_skills(tab_bar.current_tab)
-
-func show_skills(index: int):
-	clear_skills()
-	var character:= characters[index]
-	for skill in character.skill_holder.skills:
-		var button := skill_menu_button_template.instantiate() as SkillMenuButton
-		button.initialize(skill, skill_description_label)
-		skill_container.add_child(button)
 
 func add_tabs_per_character():
 	tab_bar.clear_tabs()
 	for character in characters:
-		tab_bar.add_tab(character.char_name)
+		var tab_name = "[" + character.pc_class.localization_name + "] " + character.char_name
+		tab_bar.add_tab( tab_name )
+
+func on_visibility_changed():
+	if (visible):
+		render_tab( tab_bar.current_tab )
+
+func render_tab(index: int):
+	var character:= characters[index]
+	show_skills(character)
+	set_available_skill_points_label(character)
+
+func show_skills(character: PlayerCombatant):
+	clear_skills()
+	for skill in character.skill_holder.skills:
+		var button := skill_menu_button_template.instantiate() as SkillMenuButton
+		button.initialize( skill )
+		button.disabled = character.available_skill_points == 0
+		skill_container.add_child( button )
+
+func set_available_skill_points_label(character: PlayerCombatant):
+	skill_points_label.text = "Available skill points: " + str(character.available_skill_points)
 
 func clear_skills():
 	for child in skill_container.get_children():
-		skill_container.remove_child(child)
+		skill_container.remove_child( child )
 		child.queue_free()
--- Scripts/UI/Skill/SkillMenuButton.gd
+++ Scripts/UI/Skill/SkillMenuButton.gd
@@ -1,23 +1,15 @@
 class_name SkillMenuButton extends TextureButton
 
 var skill: SkillInstance
-var description_label: RichTextLabel
+@export var level_label: Label
 
-func initialize(_skill: SkillInstance, _description_label: RichTextLabel):
+func initialize(_skill: SkillInstance):
 	skill = _skill
-	description_label = _description_label
 	texture_normal = skill.monitored_skill.display_texture
-	description_label.hide()
-
-func _ready():
-	button_down.connect(show_description)
-
-func show_description():
-	description_label.text = get_formatted_skill_text()
-	description_label.show()
+	tooltip_text = get_formatted_skill_text()
+	level_label.text = str(skill.current_upgrade_level) + "/" + str(skill.monitored_skill.max_rank)
 
 func get_formatted_skill_text() -> String:
-	var text = "[b]" + skill.monitored_skill.localization_name + "[/b]"
-	text += "\n"
+	var text = skill.monitored_skill.localization_name + ": "
 	text += skill.monitored_skill.localization_description
 	return text
