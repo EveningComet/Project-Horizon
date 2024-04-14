8e69721552152714f3ba1f962a2f7105fa2104e8
thanh <33893929+yenthanhmm5@users.noreply.github.com>
2024-04-14 18:43:14 +0200

    Improve readability + consistent spacing

--- Scripts/UI/Skill/SkillsTreeRenderer.gd
+++ Scripts/UI/Skill/SkillsTreeRenderer.gd
@@ -1,10 +1,10 @@
-# Spawns and draws the skills tree
+## This node spawns the skills tree
+## by spawning the buttons and their connecting lines
 class_name SkillsTreeRenderer extends Node
 
 @export var skill_container: HBoxContainer
 @export var skill_menu_button_template: PackedScene
 @export var wait_skills_render_timer: Timer
-
 @export var skills_group_name : String = "skills"
 
 # Dictionary { skill_data : skill_menu_button }
@@ -14,6 +14,7 @@ var on_skill_upgraded: Callable
 var skill_points_depleted: Signal
 
 func _ready():
+	# The buttons must first render before we can get their position for drawing lines
 	wait_skills_render_timer.timeout.connect( finish )
 
 func initialize(_on_skill_upgraded: Callable, _skill_points_depleted: Signal):
@@ -25,36 +26,40 @@ func start(skills: Array):
 	for skill in skills:
 		var button:= make_skill_button( skill ) as SkillMenuButton
 		button_per_skill[skill.monitored_skill] = button
+		fade_in( button )
 		skill_container.add_child( button )
-	# Buttons need to render before their `position` property is correct
 	wait_skills_render_timer.start() 
 
 func finish():
 	for skill_data in button_per_skill.keys():
 		if (not skill_data.unlockable.is_empty()):
-			draw_lines_to_all_unlockables(button_per_skill[skill_data])
+			draw_lines_to_all_unlockables_from( button_per_skill[skill_data] )
 
-func draw_lines_to_all_unlockables(button: SkillMenuButton):
+func draw_lines_to_all_unlockables_from(button: SkillMenuButton):
 	var skill_data := button.skill.monitored_skill
 	var unlockables := skill_data.unlockable
 	for unlockable in unlockables:
 		var target := button_per_skill[unlockable] as SkillMenuButton
 		var line := spawn_connecting_line(button, target)
-		fade_in_line(line)
+		fade_in( line )
 		skill_container.add_child(line)
 
-func spawn_connecting_line(from: SkillMenuButton, to: SkillMenuButton) -> Line2D:
+func spawn_connecting_line(begin: SkillMenuButton, end: SkillMenuButton) -> Line2D:
 	var line := Line2D.new()
-	line.add_point(from.position + (from.size / 2))
-	line.add_point(to.position + (to.size / 2))
+	line.add_point( center_position( begin ) )
+	line.add_point( center_position( end ) )
 	line.z_index = -1 # push line to background
 	line.default_color = Color(125, 125, 125)
 	return line
 
-func fade_in_line(line: Line2D):
-	line.modulate = Color(1, 1, 1, 0)
+func center_position(item: CanvasItem):
+	return item.position + item.size / 2
+
+func fade_in(item: CanvasItem):
+	var target_modulate := item.modulate
+	item.modulate = Color(1, 1, 1, 0)
 	var tween:= get_tree().create_tween()
-	tween.tween_property(line, "modulate", Color(1, 1, 1, 1), 0.5)
+	tween.tween_property( item, "modulate", target_modulate , 0.5 )
 
 func make_skill_button(skill: SkillInstance) -> SkillMenuButton:
 	var button := skill_menu_button_template.instantiate() as SkillMenuButton
