33f2e64ca764de3cc0f5339756c9995c2ae1efe0
thanh <yenthanhmm5@gmail.com>
2024-04-18 20:54:51 +0200

    Enforce lower and upper bounds for the attribute menu

--- Scripts/UI/Skill/AttributeMenuEntry.gd
+++ Scripts/UI/Skill/AttributeMenuEntry.gd
@@ -4,36 +4,57 @@ class_name AttributeMenuEntry extends HBoxContainer
 @export var upgrade_button: Button
 @export var downgrade_button: Button
 
+signal attribute_upgraded
+signal attribute_downgraded
+
 var stat_types := StatTypes.new()
 var attribute: StatTypes.stat_types
 
 var stat: Stat
-var draft_stat: Stat = null
+var draft_points: int = 0
 var increase_amount: int = 0
 
-func initialize(_attribute: StatTypes.stat_types, _character_changed: Signal):
+func initialize(
+	_attribute: StatTypes.stat_types,
+	_character_changed: Signal, _points_depleted: Signal, _points_available: Signal):
 	attribute = _attribute
 	_character_changed.connect( update_and_render )
+	_points_depleted.connect( disable_upgrade )
+	_points_available.connect( enable_upgrade)
 	upgrade_button.button_down.connect( upgrade )
 	downgrade_button.button_down.connect( downgrade )
 
 func update_and_render(character: PlayerCombatant):
 	stat = character.stats[attribute]
-	draft_stat = character.stats[attribute]
+	draft_points = character.stats[attribute].get_base_value()
 	increase_amount = character.get_attributes_increase()[attribute]
 	set_draft_points( stat.get_base_value() )
 
 func upgrade():
-	set_draft_points ( draft_stat.get_base_value() + increase_amount )
+	set_draft_points ( draft_points + increase_amount )
+	emit_signal("attribute_upgraded")
 
 func downgrade():
-	set_draft_points ( draft_stat.get_base_value() - increase_amount )
+	var new_amount := draft_points - increase_amount
+	if (new_amount >= stat.get_base_value()):
+		set_draft_points ( draft_points - increase_amount )
+		emit_signal("attribute_downgraded")
 
 func set_draft_points(points: int):
-	draft_stat.set_base_value( points )
+	draft_points = points
 	update_label()
+	disable_downgrade_if_minimum_reached()
+
+func enable_upgrade():
+	upgrade_button.disabled = false
+
+func disable_upgrade():
+	upgrade_button.disabled = true
+	
+func disable_downgrade_if_minimum_reached():
+	downgrade_button.disabled = draft_points <= stat.get_base_value()
 
 func update_label():
 	attribute_label.text = stat_types.stat_types_to_string[attribute]
 	attribute_label.text += ": "
-	attribute_label.text += "-" if draft_stat == null else str( draft_stat.get_base_value() )
+	attribute_label.text += str(draft_points)
--- Scripts/UI/Skill/AttributesMenu.gd
+++ Scripts/UI/Skill/AttributesMenu.gd
@@ -4,8 +4,13 @@ class_name AttributesMenu extends Node
 @export var container: VBoxContainer
 @export var menu_entry_template: PackedScene
 
+signal attribute_points_depleted
+signal attribute_points_available
+
 var character: PlayerCombatant
 var entries: Array[AttributeMenuEntry]
+var draft_points: int = 0
+
 var character_changed: Signal
 
 func initialize(_character_changed: Signal):
@@ -15,15 +20,32 @@ func initialize(_character_changed: Signal):
 	
 func update_and_render(_character: PlayerCombatant):
 	character = _character
-	render()
+	set_draft_points( character.available_attribute_points )
 
-func render():
+func attribute_upgraded():
+	set_draft_points( draft_points - 1)
+
+func attribute_downgraded():
+	set_draft_points( draft_points + 1)
+
+func set_draft_points(points: int):
+	draft_points = points
+	if (draft_points == 0):
+		emit_signal("attribute_points_depleted")
+	elif (draft_points > 0):
+		emit_signal("attribute_points_available")
+	update_points_label()
+	
+func update_points_label():
 	points_label.text = "Available attribute points: "
-	points_label.text += str(character.available_attribute_points)
+	points_label.text += str(draft_points)
 
 func spawn_entries_for_attributes():
 	for attribute in StatTypes.new().attributes():
-		var entry := menu_entry_template.instantiate()
-		entry.initialize(attribute, character_changed)
+		var entry := menu_entry_template.instantiate() as AttributeMenuEntry
+		entry.initialize(
+			attribute, character_changed, attribute_points_depleted, attribute_points_available)
+		entry.attribute_upgraded.connect( attribute_upgraded )
+		entry.attribute_downgraded.connect( attribute_downgraded )
 		entries.append(entry)
 		container.add_child(entry)
