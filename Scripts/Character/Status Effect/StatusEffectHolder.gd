5d5590a5ef70a6cc2e96f0e98cce6454ee779ee1
thannicko <thanh@home>
2024-04-28 19:53:18 +0200

    Added handling of on tick and on expire.

--- Scripts/Character/Status Effect/StatusEffectHolder.gd
+++ Scripts/Character/Status Effect/StatusEffectHolder.gd
@@ -19,17 +19,22 @@ func add_status_effect(status_to_add: StatusEffect) -> void:
 	combat_data.on_expire_action_mediator = to_mediator(status_to_add.on_expire)
 	apply_if_not_null(combat_data.on_apply_action_mediator)
 	statuses[status_to_add] = combat_data
-	print(monitored_combatant, ": added status ", status_to_add.localization_name, " for ", status_to_add.duration_in_turns)
+	print(monitored_combatant, ": added status ", status_to_add.localization_name, " for ", status_to_add.duration_in_turns, " turns")
 
-func apply_status_effect(status: StatusEffect) -> void:
-	print(monitored_combatant, ": ticked status ", status.localization_name, " for ", status.duration_in_turns)
-	var combat_data := statuses[status] as StatusEffectCombatData
-	apply_if_not_null(combat_data.on_tick_action_mediator)
-	combat_data.duration -= 1
-
+func apply_status_effects() -> void:
+	for status in statuses:
+		var combat_data := statuses[status] as StatusEffectCombatData
+		if (combat_data.duration <= 0):
+			remove_status_effect(status)
+		else:
+			apply_if_not_null(combat_data.on_tick_action_mediator)
+			combat_data.duration -= 1
+			print(monitored_combatant, ": ticked status ", status.localization_name, ", turns left: ", combat_data.duration)
 
 func remove_status_effect(status_to_remove: StatusEffect) -> void:
+	apply_if_not_null(statuses[status_to_remove].on_expire_action_mediator)
 	statuses.erase(status_to_remove)
+	print(monitored_combatant, ": removed status ", status_to_remove.localization_name)
 	
 func apply_if_not_null(action_mediator: ActionMediator):
 	if (action_mediator != null):
