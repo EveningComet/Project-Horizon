842dfaf81d4ff3e9262d66560fa15bfb267a100e
thannicko <thanh@home>
2024-04-28 18:53:29 +0200

    Revert "Use `StatModifier` for damage"
    This reverts commit 2da4424dc1012b558417fb8f604f7f5850eaaf63.

--- Scripts/Character/Status Effect/Status Effect Data/StatusEffectData.gd
+++ Scripts/Character/Status Effect/Status Effect Data/StatusEffectData.gd
@@ -1,4 +1,4 @@
 ## Base class that describes what a status effect does.
 class_name StatusEffectData extends Resource
 
-@export var damage_data: StatModifier
+@export var damage_to_inflict: int = 0
