67b6bfef78005575401cc5e691e6a53b48e486b6
thannicko <thanh@home>
2024-05-01 11:50:28 +0200

    Prevent adding an existing status effect

--- Scripts/Character/Status Effect/StatusEffectHolder.gd
+++ Scripts/Character/Status Effect/StatusEffectHolder.gd
@@ -12,9 +12,10 @@ func initialize(combatant: Combatant) -> void:
 	monitored_combatant = combatant
 	
 func add_status_effect(status_to_add: StatusEffect) -> void:
-	statuses[status_to_add] = status_to_add.duration_in_turns
-	status_to_add.trigger_on_apply(monitored_combatant)
-	print(monitored_combatant, ": added status effect ", status_to_add.localization_name, " for ", status_to_add.duration_in_turns, " turns")
+	if (not statuses.has(status_to_add)):
+		statuses[status_to_add] = status_to_add.duration_in_turns
+		status_to_add.trigger_on_apply(monitored_combatant)
+		print(monitored_combatant, ": added status effect ", status_to_add.localization_name, " for ", status_to_add.duration_in_turns, " turns")
 
 func apply_status_effects() -> void:
 	for status in statuses:
