eccf045910d9c625f77fee88d8f366abf3e22646
thannicko <thanh@home>
2024-06-28 19:54:40 +0200

    Chang e to a list of on apply/tick/expire effects

--- Scripts/Character/Status Effect/StatusEffect.gd
+++ Scripts/Character/Status Effect/StatusEffect.gd
@@ -8,22 +8,22 @@ class_name StatusEffect extends Resource
 @export var is_contagious: bool
 @export_range(0.0, 1.0, .05) var chance_of_spreading: float
 @export var incubation_time: int
-@export var on_apply: StatusEffectOnApplyData
-@export var on_turn_tick: StatusEffectOnTurnTickData
-@export var on_expire: StatusEffectOnExpireData
+@export var on_apply: Array[StatusEffectOnApplyData]
+@export var on_turn_tick: Array[StatusEffectOnTurnTickData]
+@export var on_expire: Array[StatusEffectOnExpireData]
 @export var is_negative: bool = false
 @export var duration_in_turns: int = 1
 
 func trigger_on_apply(combatant: Combatant):
-	if on_apply != null:
-		on_apply.trigger(combatant)
+	for effect in on_apply:
+		effect.trigger(combatant)
 
 func trigger_on_turn_tick(combatant: Combatant, turns_elapsed: int):
-	if on_turn_tick != null:
-		on_turn_tick.trigger(combatant, turns_elapsed)
+	for effect in on_turn_tick:
+		effect.trigger(combatant, turns_elapsed)
 
 func trigger_on_expire(combatant: Combatant):
-	if on_apply != null:
-		on_apply.remove_modifiers(combatant)
-	if on_expire != null:
-		on_expire.trigger(combatant)
+	for effect in on_apply:
+		effect.remove_modifiers(combatant)
+	for effect in on_expire:
+		effect.trigger(combatant)
