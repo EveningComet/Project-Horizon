0100f1a404d2f60fefa244aa1f8ccf663c7434d4
EveningComet <165712308+EveningComet@users.noreply.github.com>
2024-08-01 17:02:57 -0400

    Main Menu Gamepad & Keyboard Controls
    Implemented the ability to control the main menu with the keyboard and gamepad.

--- Scripts/UI/General/MainMenuController.gd
+++ Scripts/UI/General/MainMenuController.gd
@@ -2,12 +2,15 @@ class_name MainMenuController extends Node
 
 @export_file("*.tscn") var homebase_scene: String
 
+@export var button_holder:   Container
 @export var new_game_button: Button
 @export var quit_button:     Button
 
 func _ready() -> void:
 	new_game_button.pressed.connect( on_new_game_button_pressed )
 	quit_button.pressed.connect( on_quit_button_pressed )
+	
+	button_holder.get_child(0).grab_focus()
 
 func on_new_game_button_pressed() -> void:
 	SceneController.switch_to_scene(
